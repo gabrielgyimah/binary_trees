9201122d1060bf72fc96f2ed9ce14bcbc8bb7f1a
gabrielgyimah <gabrielamanfogyimah@gmail.com>
2023-08-03 21:53:23 +0100

    Added a function that finds the sibling of a node

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -3,7 +3,7 @@
 /**
  * binary_tree_sibling - finds the sibling of a node
  * @node:  pointer to the node to find the sibling
- * Returns: pointer to the siblin node
+ * Return: pointer to the siblin node
 */
 
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
